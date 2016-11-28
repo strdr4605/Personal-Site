3dd88c92f3e1d384744f5fbb7e9137aeb7e7cc87
Dragos Strainu <dstrainu@YOPESOs-Mac-mini-9.local>
2016-11-28 16:23:42 +0200

    Change project structure(add dist and src folders). Add packages gulp-autoprefixer, gulp-imagemin. On about.html set text to wrap around the about-photo.

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,30 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const imagemin = require('gulp-imagemin');
+
+
+gulp.task('imagemin', () =>
+    gulp.src('./assets/src/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./assets/dist/images'))
+);
 
 gulp.task('sass', function () {
-  return gulp.src('./assets/sass/**/*.scss')
+  return gulp.src('./assets/src/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./assets/css'));
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
+    .pipe(gulp.dest('./assets/dist/css'));
 });
 
 gulp.task('sass:watch', function () {
-  gulp.watch('./assets/sass/**/*.scss', ['sass']);
+  gulp.watch('./assets/src/sass/**/*.scss', ['sass']);
 });
+
+gulp.task('default', ['imagemin', 'sass', 'sass:watch']);
+
+// autoprefixer gulp, imagemin gulp
