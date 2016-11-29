e13fe8b18ae0c587d06ecef5495fb92cbaf47669
Dragos Strainu <dstrainu@YOPESOs-Mac-mini-9.local>
2016-11-29 16:21:16 +0200

    Add view zoom-product for first product

--- assets/src/js/my-script.js
+++ assets/src/js/my-script.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
-    $(".product").click(function(){
-        $(this).hide();
-        console.log('click')
+    $('.product').click(function(event){
+      event.stopPropagation()
+      $(this).children('.zoom-product').addClass('display-flex').removeClass('hidden')
     });
+    $('body').click(function () {
+      $('.display-flex').addClass('hidden').removeClass('display-flex')
+    })
 });
